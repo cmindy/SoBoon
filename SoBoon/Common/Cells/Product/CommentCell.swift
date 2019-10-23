307feeadda357e2afe84c02f8021b083c9ae9d6a
cmindy <cmindy.soft@gmail.com>
2019-10-24 02:26:11 +0900

    feat: Add CommentCell

---
+++ SoBoon/Common/Cells/Product/CommentCell.swift
@@ -0,0 +1,55 @@
+//
+//  CommentCell.swift
+//  SoBoon
+//
+//  Created by CHOMINJI on 24/10/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    // MARK: - Vars
+    
+    @IBOutlet weak var baseView: UIView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
+    private func initVars() {
+        self.clipsToBounds = true
+    }
+    
+    private func initBackgroundView() {
+        baseView.backgroundColor = kWHITE
+    }
+    
+    private func initProfileImageView() {
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.backgroundColor = kGRAY_200
+        profileImageView.roundCorners()
+    }
+    
+    private func initLabels() {
+        nameLabel.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        
+        commentLabel.textColor = kGRAY
+        commentLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        
+        dateLabel.textColor = kGRAY_LIGHT
+        dateLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        initVars()
+        initBackgroundView()
+        initProfileImageView()
+    }
+
+}
