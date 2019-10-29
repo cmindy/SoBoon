5d0eeb5948666dbf64019513d4a4fddc2601a8af
cmindy <cmindy.soft@gmail.com>
2019-10-29 22:12:52 +0900

    feat: Add UIFont extension that help easily using custom font

---
+++ SoBoon/Common/Extensions/UIFont+Custom.swift
@@ -0,0 +1,37 @@
+//
+//  UIFont+Custom.swift
+//  SoBoon
+//
+//  Created by CHOMINJI on 29/10/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+enum CustomFontName {
+    static let notoSansCJKkr = "NotoSansCJKkr"
+}
+
+enum NotoSansWeight: String {
+    case thin = "-Thin"
+    case light = "-Light"
+    case demiLight = "-DemiLight"
+    case regular = "-Regular"
+    case medium = "-Medium"
+    case bold = "-Bold"
+    case black = "-Black"
+}
+
+extension UIFont {
+    static func notoSansFont(of size: CGFloat = UIFont.systemFontSize, weight: NotoSansWeight = .regular) -> UIFont {
+        return UIFont(name: "\(CustomFontName.notoSansCJKkr)\(weight.rawValue)", size: size)!
+    }
+    
+    var isBold: Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitBold)
+    }
+
+    var isItalic: Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitItalic)
+    }
+}
